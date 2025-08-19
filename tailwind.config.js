dc2acc7052620f30149b584c383201de911e70ff
MoeinGanji2568 <moeinganji2004@gmail.com>
2025-08-19 19:27:00 +0330

    Add custom color palette to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        C_gray: {
+          50: "#F2F2F2",
+          100: "#D9D9D9",
+          200: "#808080",
+          300: "#333333",
+          400: "#262626",
+          500: "#1A1A1A",
+          600: "#0D0D0D",
+        },
+        C_primary: {
+          100: "#8284FA",
+          200: "#5E60CE",
+        },
+        C_blue: {
+          100: "#4EA8DE",
+          200: "#1E6F9F",
+        },
+        C_red: {
+          100: "#E25858",
+        },
+      },
+    },
   },
   plugins: [],
 };
