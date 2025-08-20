242b898b95270facc960e529bf76a05d4cb9356e
MoeinGanji2568 <moeinganji2004@gmail.com>
2025-08-20 15:12:09 +0330

    Add TodoHeader component for displaying the todo list title

---
+++ src/components/todos/TodoHeader.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "../ui/typography";
+
+const TodoHeader = () => {
+  return (
+    <div className="bg-C_gray-600 h-[200px]">
+      <Typography variant="h3" className="text-center text-C_primary-100 p-3">
+        Moein Mohsenzadeh - Todo List
+      </Typography>
+    </div>
+  );
+};
+
+export default TodoHeader;
