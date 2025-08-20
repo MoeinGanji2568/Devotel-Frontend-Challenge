840a37362b56e8be0e1e53df99c149deabd405ec
MoeinGanji2568 <moeinganji2004@gmail.com>
2025-08-20 15:10:42 +0330

    Refactor TodoCard component to use TypeScript types, integrate DeleteTodo component, and enhance checkbox functionality with todo state management.

--- src/components/todos/TodoCard.tsx
+++ src/components/todos/TodoCard.tsx
@@ -1,17 +1,32 @@
-import { Trash } from "lucide-react";
 import { Typography } from "../ui/typography";
+import type { Todo } from "../../types/todo/todo.types";
+import type { FC } from "react";
+import DeleteTodo from "./DeleteTodo";
 
-const TodoCard = () => {
+interface TodoCardProps {
+  todo: Todo;
+}
+
+const TodoCard: FC<TodoCardProps> = ({ todo }) => {
   return (
     <div className="bg-C_gray-400 p-3 rounded-lg border border-C_gray-300 max-w-full flex  justify-between">
       <div className="flex gap-5 items-start">
-        <input type="checkbox" className="mt-2" />
-        <Typography variant="p" className="text-C_gray-100 max-w-[90%]">
-          Integer urna interdum massa libero auctor neque turpis turpis semper.
-          Duis vel sed fames integer.
+        <input
+          type="checkbox"
+          className="mt-2"
+          checked={todo?.completed}
+          onChange={() => {}}
+        />
+        <Typography
+          variant="p"
+          className={`text-C_gray-100 max-w-[90%] ${
+            todo?.completed ? "line-through" : ""
+          }`}
+        >
+          {todo?.todo}
         </Typography>
       </div>
-      <Trash className="size-5 text-C_gray-200 cursor-pointer mt-2" />
+      <DeleteTodo todoId={todo.id} />
     </div>
   );
 };
