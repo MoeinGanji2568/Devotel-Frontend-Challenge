82e48a7ed8c424177706aef434b444e6bf91708d
MoeinGanji2568 <moeinganji2004@gmail.com>
2025-08-21 10:24:10 +0330

    Add FilterFormData and TodoCardProps interfaces to enhance type safety for filtering and drag-and-drop functionality in the todo application.

--- src/types/todo/todo.types.ts
+++ src/types/todo/todo.types.ts
@@ -4,3 +4,17 @@ export interface Todo {
   completed: boolean;
   userId: number;
 }
+export interface FilterFormData {
+  search: string;
+  status: string | boolean;
+}
+
+export interface TodoCardProps {
+  todo: Todo;
+  index: number;
+  onDragStart: (event: React.DragEvent<HTMLDivElement>, index: number) => void;
+  onDragOver: (event: React.DragEvent<HTMLDivElement>, index: number) => void;
+  onDrop: (event: React.DragEvent<HTMLDivElement>, index: number) => void;
+  onDragEnd?: () => void;
+  isDragOver?: boolean;
+}
